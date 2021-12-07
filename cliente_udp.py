fbe8dc8eec4a2b11daa562316c4eeb61953529e5
Joao Guilherme <guilherme_joao@id.uff.br>
2021-12-07 02:28:36 -0300

    Gerando arquivo WAV

--- cliente_udp.py
+++ cliente_udp.py
@@ -3,46 +3,71 @@ import cv2, imutils
 import numpy as np
 import time
 import base64 #Converte dados de imagem em formato texto
+import os
+
+#==============================#
+#=           GET_IP           =#
+#==============================#
+
+def getIP():
+    return socket.gethostbyname(socket.gethostname())
+
+def getHostName():
+    return socket.gethostname()
+
+
+#==============================#
+#=       Inicializações       =#
+#==============================#
 
 BUFFER_SIZE = 65536
 UDP_PORT = 6000
+BREAK = False
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #Socket UDP do cliente
 client_socket.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,BUFFER_SIZE)
-host_name = socket.gethostname()
 
-#Pegar host dinâmicamente
-host_ip = socket.gethostbyname(socket.gethostname())
-#host_ip = '192.168.1.106' comentado
-print(host_ip)
+host_ip = getIP() #Pegar host_ip e host_name dinamicamente
+host_name = getHostName()
+print("host_ip: ",host_ip)
+print("host_name: ",host_name)
 
-message = b"Hello" #Mensagem enviada ao servidor
+message = b"Hello, I'm client" #Mensagem enviada ao servidor
 client_socket.sendto(message,(host_ip,UDP_PORT))
 
-#Receber datagrama de dados do servidor, no lado do cliente
-while True:
-    packet,_ = client_socket.recvfrom(BUFFER_SIZE)
-    data = base64.b64decode(packet,' /') #Fazendo decode do datagrama recebido
-    npdata = np.fromstring(data,dtype=np.uint8)
-    frame = cv2.imdecode(npdata,1)
-    cv2.imshow("Transmissao",frame) #Display do video no cliente
+def receive_video():
+    cv2.namedWindow("Video no Cliente")
+    cv2.moveWindow("Video no Cliente",10,360)
+    fps,st,frames_to_count,cnt = (0,0,200,0)
 
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord('q'):
-        client_socket.close()
-        break
+    #Receber datagrama de dados do servidor, no lado do cliente
+    while True:
+        packet,_ = client_socket.recvfrom(BUFFER_SIZE)
+        data = base64.b64decode(packet,' /') #Fazendo decode do datagrama recebido
+        npdata = np.fromstring(data,dtype=np.uint8)
+        frame = cv2.imdecode(npdata,1)
+        cv2.imshow("Video no Cliente",frame) #Display do video no cliente
+
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord('q'):
+            client_socket.close()
+            os.exit(1)
+
+        if cnt == frames_to_count:
+            try:
+                fps = round(frames_to_count/(time.time()-st))
+                st = time.time()
+                cnt = 0
+            except:
+                cnt += 1
 
 
-''' COMENTADO
-HOST = '127.0.0.1'
-PORT_UDP = 6000
-BUFFER_SIZE = 1024
-
-#Conexão com Servidor de Streaming por UDP na porta 6000
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp:
-    udp.sendto(b'Hello from client', (HOST, PORT_UDP))
-    data = udp.recv(BUFFER_SIZE)
-print(data.decode('utf-8'))
+def receive_audio():
+    pass
 
 
-'''
+#Executa paralelamente as 2 funções com Thread
+from concurrent.futures import ThreadPoolExecutor
+with ThreadPoolExecutor(max_workers=2) as executor:
+    executor.submit(receive_audio)
+    executor.submit(receive_video)
--- servidor_streaming.py
+++ servidor_streaming.py
@@ -3,56 +3,122 @@ import cv2, imutils
 import time
 import numpy as np
 import base64 #Converte dados de imagem em formato texto
+import os #funcões DE INTERFACE COM O SISTEMA OPERACIONAL
+import queue
+import wave #manipular audio
 
+#Fila dos frames
+q = queue.Queue(maxsize=10)
+
+#==============================#
+#= Gerar arquivo de Audio wav =#
+#==============================#
+
+name_Video = "./Videos/Sony_Demo_720p.mp4"
+command = "ffmpeg -i {} -ab 160k -ac 2 -ar 44100 -vn {}".format(name_Video,"audio.wav")
+os.system(command)
+
+#==============================#
+#=           GET_IP           =#
+#==============================#
+
+def getIP():
+    return socket.gethostbyname(socket.gethostname())
+
+def getHostName():
+    return socket.gethostname()
+
+#==============================#
+#=       Inicializações       =#
+#==============================#
 HOST = ''
 UDP_PORT = 6000
 BUFFER_SIZE = 65536
-
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #Socket UDP do servidor
 server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,BUFFER_SIZE)
-host_name = socket.gethostname()
 
-#Pegar host dinâmicamente
-host_ip = socket.gethostbyname(socket.gethostname())
-#host_ip = '192.168.1.106' #Host local - comentado
-
+host_ip = getIP() #Pegar host_ip e host_name dinamicamente
+host_name = getHostName()
 print("host_ip: ",host_ip)
+print("host_name: ",host_name)
+
 socket_address = (host_ip,UDP_PORT)
 server_socket.bind(socket_address) #Iniciando servidor no socket_address
 print("Ouvindo em: ",socket_address)
 
+
+#==============================#
+#=       Tratando Video       =#
+#==============================#
 video = cv2.VideoCapture("./Videos/Sony_Demo_720p.mp4") #Captura do video
+FPS = video.get(cv2.CAP_PROP_FPS) #Pegando FPS do video original
+global TS
+TS = (0.5/FPS)
+BREAK = False
+print("FPS:",FPS," TS:",TS)
+totalNoFrames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+durationInSeconds = float(totalNoFrames) / float(FPS)
+d = video.get(cv2.CAP_PROP_POS_MSEC)
+print(durationInSeconds,d)
 
-while True: #Loop para o servidor receber dados de cada cliente no endereço socket
-    msg,client_addr = server_socket.recvfrom(BUFFER_SIZE) #Recebendo mensagem msg do cliente
-    print("Conexão recebida de: ",client_addr)
-    print("Mensagem recebida: ",msg)
 
-    #Enviar imagens ao cliente
-    WIDTH = 400 #Caber em um único datagrama
+def video_stream_generator():
+    WIDTH = 400
     while(video.isOpened()):
-        _,frame = video.read()
-        frame = imutils.resize(frame,width=WIDTH)
-        encoded,buffer = cv2.imencode('.jpg',frame,[cv2.IMWRITE_JPEG_QUALITY,80]) #Colocar/Encode a imagem em JPEG com qualidade de 80% após o resize
-        message = base64.b64encode(buffer) #Converter dado binario em texto e vice versa com base64
-        server_socket.sendto(message,client_addr) #Envio da mensagem ao cliente
-
-        cv2.imshow("Transmitindo video",frame) #Display da janela de transmissão no servidor
-        key = cv2.waitKey(1) & 0xFF
-        if key == ord('q'):
-            server_socket.close()
-            break
+        try:
+            _,frame = video.read()
+            frame = imutils.resize(frame,width=WIDTH)
+            q.put(frame)
+        except:
+            os._exit(1) #fecha transmissao
+    print("Transmissão Fechada")
+    BREAK = True
+    video.release()
 
 
+def video_stream():
+    global TS
+    fps,st,frames_to_count,cnt = (0,0,1,0)
 
-''' COMENTADO
-#Servidor de Streaming na porta 6000
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp:
-    udp.bind((HOST, UDP_PORT))
+    while True: #Loop para o servidor receber dados de cada cliente no endereço socket
+        msg,client_addr = server_socket.recvfrom(BUFFER_SIZE) #Recebendo mensagem msg do cliente
+        print("Conexão recebida de: ",client_addr)
+        print("Mensagem recebida: ",msg)
 
-    while True:
-        data, client = udp.recvfrom(BUFFER_SIZE)
-        udp.sendto(b'Hello world', client)
-        print(data.decode('utf-8'))
+        #Enviar imagens ao cliente
+        WIDTH = 400 #Caber em um único datagrama
+        while(True):
+            frame = q.get()
+            encoded,buffer = cv2.imencode('.jpeg',frame,[cv2.IMWRITE_JPEG_QUALITY,80]) #Colocar/Encode a imagem em JPEG com qualidade de 80% após o resize
+            message = base64.b64encode(buffer) #Converter dado binario em texto e vice versa com base64
+            server_socket.sendto(message,client_addr) #Envio da mensagem ao cliente
+            
+            #Controle de frames ao enviar a 'message'    
+            if cnt == frames_to_count:
+                try:
+                    fps = (frames_to_count/(time.time()-st))
+                    st = time.time()
+                    cnt = 0
+                    if fps > FPS:
+                        TS += 0.001
+                    elif fps < FPS:
+                        TS -=0.001
+                    else:
+                        pass
+                except:
+                    pass
+            cnt +=1
+            cv2.waitKey(int(1000*TS)) & 0xFF
+            print(TS)
+            
 
-'''
+def audio_stream():
+    pass
+
+
+#Executa paralelamente as 3 funções com Thread
+from concurrent.futures import ThreadPoolExecutor
+with ThreadPoolExecutor(max_workers=3) as executor:
+    executor.submit(audio_stream)
+    executor.submit(video_stream_generator)
+    executor.submit(video_stream)
